7cd4bbf84f2443687bdf9e320b5753c5c88039a6
DA-WN2 <adilshancp8943@gmail.com>
2026-03-24 14:37:27 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import ShippingPage from "./pages/ShippingPage";
